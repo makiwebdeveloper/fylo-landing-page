fcf52db461e552b75a1f9a5cf545d972c55ebc09
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-02-21 12:37:51 +0000

    Header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,31 @@
 import { FC } from "react";
+import PresentationImg from "../assets/illustration-intro.png";
+import BgCurvy from "../assets/bg-curvy-desktop.svg";
+import { Button } from "../ui";
 
 const Header: FC = () => {
-  return <header>Header</header>;
+  return (
+    <header className="bg-background-100 min-h-screen p-10 relative">
+      <div className="relative z-[1] md:w-[600px] mx-auto text-center">
+        <img src={PresentationImg} alt="presentation" />
+        <h1 className="text-white text-4xl font-semibold mt-10">
+          All your files in one secure location,
+          <br /> accessible anywhere
+        </h1>
+        <p className="text-white opacity-90 w-[90%] mx-auto tracking-wider mt-10">
+          Fylo stores all your most important files in one secure location.
+          <br />
+          Access them wherever you need, share and collaborate with friends
+          famoly, and co-workers.
+        </p>
+        <Button styles="mt-5">Get Started</Button>
+      </div>
+
+      <div className="absolute bottom-0 left-0 z-[0]">
+        <img src={BgCurvy} className="pointer-events-none w-full" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,9 +1,17 @@
 import { FC } from "react";
 
-interface Props {}
+interface Props {
+  styles?: string;
+}
 
-const Button: FC<Props> = ({ children }) => {
-  return <button>{children}</button>;
+const Button: FC<Props> = ({ children, styles }) => {
+  return (
+    <button
+      className={`bg-gradient-to-r from-cyan-400 to-cyan-600 min-w-[200px] font-semibold tracking-wider text-sm px-4 py-3 rounded-full text-white ${styles}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
